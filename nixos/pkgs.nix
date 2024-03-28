1e02cd2c905cc68a0f9ef5310b3ea834770d17c0
Berkano <dkopijka@gmail.com>
2024-03-28 22:33:30 +0200

    четвер, 28 березня 2024 22:33:30 +0200

--- nixos/pkgs.nix
+++ nixos/pkgs.nix
@@ -1,56 +1,56 @@
 {pkgs, ...}:
-
 with pkgs; [
-	
-	firefox
-	kate
-	discord
-	tdesktop
-	neofetch
-	easyeffects
-	xclip
-	steam
-	git
-	alacritty
-	piper
-	ranger
-	rofi
-	fzf
-	btop
-	xorg.xrandr
-	arandr
-	eza
-	bat
-	fd
-	zoxide
-	onlyoffice-bin
-	ripgrep
-	vesktop
-	du-dust
-	lazygit
-	ffmpeg
-	wget
-	file
-	rofi-wayland
-	obsidian
-	swww
-	grim
-   	slurp
-    wl-clipboard
-	alejandra
-	nil
-	(pkgs.waybar.overrideAttrs (oldAttrs: {
-        mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
-      	})
-    	)	
+  firefox
+  kate
+  discord
+  tdesktop
+  neofetch
+  easyeffects
+  xclip
+  steam
+  git
+  alacritty
+  piper
+  ranger
+  rofi
+  fzf
+  btop
+  xorg.xrandr
+  arandr
+  eza
+  bat
+  fd
+  zoxide
+  onlyoffice-bin
+  ripgrep
+  vesktop
+  libnotify
+  du-dust
+  lazygit
+  ffmpeg
+  wget
+  file
+  rofi-wayland
+  obsidian
+  swww
+  grim
+  slurp
+  wl-clipboard
+  alejandra
+  nil
+  (
+    pkgs.waybar.overrideAttrs (oldAttrs: {
+      mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+    })
+  )
 
-	(wineWowPackages.full.override {
-     	wineRelease = "staging";
-     	mingwSupport = true;
-   	})
-	
-	stow
-	sxhkd
-	winetricks
-	bottles
+  (wineWowPackages.full.override {
+    wineRelease = "staging";
+    mingwSupport = true;
+  })
+
+  stow
+  sxhkd
+  winetricks
+  bottles
 ]
