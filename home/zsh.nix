cddbc1a4fd9d517c803351172494f53233066a4e
Berkano <dkopijka@gmail.com>
2024-03-31 15:51:15 +0300

    неділя, 31 березня 2024 15:51:15 +0300

--- home/zsh.nix
+++ home/zsh.nix
@@ -3,42 +3,43 @@
     enable = true;
   };
   programs.zsh.initExtra = ''
-          autoload -U colors && colors
-          PS1="%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[red]%}]%{$reset_color%}$%b "
-          #alias update="sudo nixos-rebuild switch --flake ~/Config#nixos"
-          alias enix="nvim ~/Config/nixos/configuration.nix"
-          update()
-          {
-          	pushd ~/Config
-          	git add -A
-          	git commit -m "$(date)"
-          	sudo nixos-rebuild switch --flake ~/Config#nixos
-          	popd
+           autoload -U colors && colors
+           PS1="%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[red]%}]%{$reset_color%}$%b "
+           #alias update="sudo nixos-rebuild switch --flake ~/.config/NixOS#nixos"
+           alias enix="nvim ~/.config/NixOS/nixos/configuration.nix"
+           update()
+           {
+           	pushd ~/.config/NixOS
+           	git add -A
+           	git commit -m "$(date)"
+    git push
+           	sudo nixos-rebuild switch --flake ~/.config/NixOS#nixos
+           	popd
+           }
+           alias epkgs="nvim ~/.config/NixOS/nixos/pkgs.nix"
+
+           EDITOR="nvim"
+
+        hst() {
+               history 0 | cut -c 8- | uniq | ${pkgs.fzf}/bin/fzf | ${pkgs.wl-clipboard}/bin/wl-copy
+           }
+
+     lfcd () {
+              tmp="$(mktemp)"
+              lf -last-dir-path="$tmp" "$@"
+              #./lfrun
+              if [ -f "$tmp" ]; then
+                  dir="$(cat "$tmp")"
+                  rm -f "$tmp"
+                  if [ -d "$dir" ]; then
+                      if [ "$dir" != "$(pwd)" ]; then
+                          cd "$dir"
+                      fi
+                  fi
+              fi
           }
-          alias epkgs="nvim ~/Config/nixos/pkgs.nix"
-
-          EDITOR="nvim"
-
-       hst() {
-              history 0 | cut -c 8- | uniq | ${pkgs.fzf}/bin/fzf | ${pkgs.wl-clipboard}/bin/wl-copy
-          }
-
-    lfcd () {
-             tmp="$(mktemp)"
-             lf -last-dir-path="$tmp" "$@"
-             #./lfrun
-             if [ -f "$tmp" ]; then
-                 dir="$(cat "$tmp")"
-                 rm -f "$tmp"
-                 if [ -d "$dir" ]; then
-                     if [ "$dir" != "$(pwd)" ]; then
-                         cd "$dir"
-                     fi
-                 fi
-             fi
-         }
-         alias lf="lfcd"
-		 alias ndz="nix develop --impure -c zsh"
+          alias lf="lfcd"
+    alias ndz="nix develop --impure -c zsh"
 
   '';
 }
